480311ee0d95dfb725d1e90044931e2486063fcf
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-26 14:39:41 +0000

    first version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # historic_cities_django
 A Django application showing the growth and location of historical cities based on the Urban Spatial Data from 3700BC to 2000AD found at https://sedac.ciesin.columbia.edu/data/set/urbanspatial-hist-urban-pop-3700bc-ad2000, and is a copy of a version done in ruby at https://github.com/scharlau/historical_cities.
 
-Use xlrd to parse excel sheet https://www.oreilly.com/library/view/data-wrangling-with/9781491948804/ch04.html 
+Use xlrd to parse excel sheet https://www.oreilly.com/library/view/data-wrangling-with/9781491948804/ch04.html - but this only reads xls files, so switched to https://openpyxl.readthedocs.io/en/stable/ 
 
 
--- cities/management/commands/parse_cities.py
+++ cities/management/commands/parse_cities.py
@@ -1,8 +1,8 @@
-import xlrd
 import os
 from pathlib import Path
 from django.db import models
 from django.core.management.base import BaseCommand, CommandError
+from openpyxl import load_workbook
 
 from cities.models import City
 
@@ -14,7 +14,23 @@ class Command(BaseCommand):
         print('table dropped')
 
         base_dir = Path(__file__).resolve().parent.parent.parent.parent
-        book_path = os.path.join(base_dir, '/cities/city_data/urbanspatial-hist-urban-pop-3700bc-ad2000-xlsx.xlsx')
-        book = xlrd.open_workbook(book_path)
-        sheet = book.sheet_by_name('Historical Urban Population')
-        print(sheet.nrows)
+        book_path = os.path.join(base_dir, 'cities/city_data/urbanspatial-hist-urban-pop-3700bc-ad2000-xlsx.xlsx')
+        book = load_workbook(book_path)
+        sheet = book['Historical Urban Population']
+        print(sheet.title)
+        max_row_num = sheet.max_row
+        max_col_num = sheet.max_column
+        print(max_row_num)
+        print(max_col_num)
+
+        # as this is a spreadsheet and not a csv file, we need to iterate cell by cell over a range of cells
+        # skip first row as headers, and skip first column as we don't need it
+        for i in range(2, max_row_num+1):
+            for j in range(2, max_col_num+1):
+                cell_obj=sheet.cell(row=i, column=j)
+                print(cell_obj.value, end='|')
+            print('\n')
+
+
+            
+        
